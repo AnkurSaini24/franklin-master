d9c4496ddabf5dd1af7e2c45fba249b1dbe9b475
Ankur Kumar Saini <65290491+AnkurSaini24@users.noreply.github.com>
2020-11-12 11:13:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,28 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var Port = process.env.PORT || 8100;
 
-
-app.use('/', indexRouter);
-app.use('/ideas', ideasRouter);
-app.use('/gcptasks', gcpTasksRouter);
-app.use('/demo',demoRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.listen(Port,()=>{
   console.log(`app listening on port ${Port}`);
 });
